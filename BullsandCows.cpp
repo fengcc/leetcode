1b512563951e98348177695f9deef84f5fcc41ff
Feng Cheng <fengchengcf@gmail.com>
2016-03-02 20:21:46 +0800

    Modified to  a better method

--- BullsandCows.cpp
+++ BullsandCows.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        multiset<char> s, g;
-        multiset<char>::iterator it;
-        int count_A = 0, count_B = 0;
         int n = secret.length();
+        int flags[10] = {0};
+        int bulls = 0, cows = 0;
         
         for (int i = 0; i < n; ++i) {
             if (secret[i] == guess[i])
-                ++count_A;
+                ++bulls;
             else {
-                if ((it = g.find(secret[i])) != g.end()) {
-                    ++count_B;
-                    g.erase(it);
-                } else 
-                    s.insert(secret[i]);
-                    
-                if ((it = s.find(guess[i])) != s.end()) {
-                    ++count_B;
-                    s.erase(it);
-                } else
-                    g.insert(guess[i]);
+                if (flags[secret[i] - '0']++ < 0) ++cows;
+                if (flags[guess[i] - '0']-- > 0) ++cows;
             }
         }
-        char ret[5];
-        sprintf(ret, "%dA%dB", count_A, count_B);
-        return ret;
+        return to_string(bulls) + 'A' + to_string(cows) + 'B';
     }
 };
