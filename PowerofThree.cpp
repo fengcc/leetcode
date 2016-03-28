6e989077745124556d40a606381870885dfeca34
Feng Cheng <fengchengcf@gmail.com>
2016-03-28 23:56:00 +0800

    Update PowerofThree.cpp

--- PowerofThree.cpp
+++ PowerofThree.cpp
@@ -1,5 +1,6 @@
-public class Solution {
-    public boolean isPowerOfThree(int n) {
-        return Integer.toString(n, 3).matches("10*");
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        return n == 0 ? false : abs(fmod((log10(n) / log10(3)), 1)) < numeric_limits<double>::min();
     }
-}
+};
