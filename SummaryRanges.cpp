1d269e133f28c9dae864bff428b3ccfd05633859
Feng Cheng <fengchengcf@gmail.com>
2016-03-29 19:53:13 +0800

    Update SummaryRanges.cpp

--- SummaryRanges.cpp
+++ SummaryRanges.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if (nums.size() == 0) return {};
-        
         vector<string> result;
         int n = nums.size();
-        string s;
-        
-        for (int i = 0, j = 1; i < n; ) {
-            while (j < n && nums[j] == nums[j - 1] + 1) ++j;
-            
-            if (i < j - 1)
-                s = to_string(nums[i]) + "->" + to_string(nums[j - 1]);
-            else
-                s = to_string(nums[i]);
-            result.push_back(s);
-            s.clear();
-            
-            i = j;
-            ++j;
+        for (int s = 0, i = 0; i < n; ++i) {
+            if (i == n-1 || nums[i] + 1 != nums[i+1]) {
+                string str = to_string(nums[s]);
+                if (s != i) str += "->" + to_string(nums[i]);
+                result.push_back(str);
+                s = i + 1;
+            }
         }
-        
         return result;
     }
 };
