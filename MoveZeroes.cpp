0369936a2054501bd1279396a75a6025da7edff9
Feng Cheng <fengchengcf@gmail.com>
2016-03-29 10:11:53 +0800

    Update MoveZeroes.cpp

--- MoveZeroes.cpp
+++ MoveZeroes.cpp
@@ -3,9 +3,8 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size(), j = 0;
         for (int i = 0; i < n; i++)
-            if (nums[i] != 0) {
-                swap(nums[i], nums[j]);
-                j++;
-            }
+            if (nums[i] != 0)
+                nums[j++] = nums[i];
+        fill(nums.begin() + j, nums.end(), 0);
     }
 };
