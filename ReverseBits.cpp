a39ee73d44c3986534fb5b592617802d5f0f66dc
Feng Cheng <fengchengcf@gmail.com>
2016-03-29 23:57:32 +0800

    Update ReverseBits.cpp

--- ReverseBits.cpp
+++ ReverseBits.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t x = 0;
-        int count = 32;
-        while (count-- > 0) {
-            x = x*2 + n%2;
-            n /= 2;
+        for (int i = 0; i < 32; ++i) {
+            x = (x << 1) | (n & 1);
+            n >>= 1;
         }
         return x;
     }
